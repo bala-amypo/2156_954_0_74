ef6e70327f0d50a67646070d1daf798a02969f25
bala-amypo <bala@amypo.in>
2025-12-17 05:10:49 +0000

    User commit

--- src/main/java/com/example/demo/service/Studentservice.java
+++ src/main/java/com/example/demo/service/Studentservice.java
@@ -11,5 +11,16 @@ public class Studentservice{
         details.put(student.getId(),student);
         return student;
     }
-    
+    public java.util.List<StudentEntity>getAllStudentEntity(){
+        return new java.util.ArrayList<>(details.values());
+    }
+    public void deleteStudentById(int id){
+        details.remove(id);
+    }
+    public java,util.optional<StudentEntity> getStudentById(int id){
+        return java.util.optional.ofNullable(details.get(id));
+    }
+    public void insertStudent(StudentEntity student){
+        details.put(student.getid(),student);
+    }
 }
